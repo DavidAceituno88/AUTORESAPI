9b29597ca42f45becc2c208926ecc3a8f7a42221
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-09-28 20:54:52 -0300

    Create Autores.cs

---
+++ Autores.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
