b43d88cf0bed34ecc6a1c106d0494f25fbafd63b
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-09 19:11:32 -0300

    Update Autores.cs

--- Autores.cs
+++ Autores.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace WebAPIAutores.Entidades
     public class Autor
     {
         public int Id { get; set; }
+        [Required (ErrorMessage = "The field {0} is required")]
         public string Nombre { get; set; }
         public List<Libro> Libros {get; set;}  
     }
