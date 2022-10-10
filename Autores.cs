4aab4f3c00eaf85f9fcf1ce434e854a506643613
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-10 11:24:58 -0300

    Update Autores.cs

--- Autores.cs
+++ Autores.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Validations;
 
 namespace WebAPIAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        
         [Required (ErrorMessage = "The field {0} is required")]
         [StringLength(maximumLength:5, ErrorMessage = "The field {0} must have more than {1} characters")]
+        [FirstLetterUpperCaseAttribute] //Validation created in the validations folder
         public string Nombre { get; set; }
         public List<Libro> Libros {get; set;}  
     }
