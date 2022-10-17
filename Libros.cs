e662777f5d2129c587968742d5f05931ee5c5f56
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-17 19:09:52 -0300

    Update Libros.cs

--- Libros.cs
+++ Libros.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Validations;
 
 namespace WebAPIAutores.Entidades
 {
     public class Libro
     {
         public int Id { get; set; }
+        [FirstLetterUpperCaseAttribute]
         public string Titulo { get; set; }
-        public int AutorId { get; set; }
-        public Autor Autor{ get; set; }
+    
     }
 }
