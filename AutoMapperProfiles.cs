5ac21d80938a9037e5e280323466ba3d4370e111
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-18 20:25:13 -0300

    Create AutoMapperProfiles.cs

---
+++ AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using WebAPIAutores.DTOs;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Utilidades
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<AutorCreationDTO, Autor>();
+        }
+    }
+}
