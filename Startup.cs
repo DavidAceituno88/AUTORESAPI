f4c7292c4872cf88926897deccd214c5e2080d16
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-18 20:24:29 -0300

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.MiddleWare;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
 
 namespace WebAPIAutores
 {
@@ -14,19 +19,21 @@ namespace WebAPIAutores
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
-        {
-             //AddJsonOptions is used here to prevent the objectloop or cycles
+        {   //AddJsonOptions is used here to prevent the objectloop or cycles
             services.AddControllers().AddJsonOptions(x => 
             x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("defaultConnections")));
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             services.AddEndpointsApiExplorer();
             services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup));
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseLogResponseHttp();
             
-             app.UseLogResponseHttp();
             // Configure the HTTP request pipeline.
             if (env.IsDevelopment())
             {
