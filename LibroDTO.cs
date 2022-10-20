dbb0b77e12a0fa6b1a7db8cafc18ef876fd228e5
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-20 20:39:01 -0300

    Create LibroDTO.cs

---
+++ LibroDTO.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Validations;
+
+namespace WebAPIAutores.DTOs
+{
+    public class LibroDTO   
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+    
+    }
+}
