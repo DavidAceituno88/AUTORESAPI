d9a016e73f9ba661969f122eb513456b81734c21
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-19 22:49:49 -0300

    Update AutoMapperProfiles.cs

--- AutoMapperProfiles.cs
+++ AutoMapperProfiles.cs
@@ -13,6 +13,7 @@ namespace WebAPIAutores.Utilidades
         public AutoMapperProfiles()
         {
             CreateMap<AutorCreationDTO, Autor>();
+            CreateMap<Autor, AutorDTO>();
         }
     }
 }
