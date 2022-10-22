f20898125fafe85cac86607b1d393f31abf3a502
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-21 21:04:05 -0300

    Update Libros.cs

--- Libros.cs
+++ Libros.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Validations;
@@ -10,7 +11,8 @@ namespace WebAPIAutores.Entidades
     {
         public int Id { get; set; }
         [FirstLetterUpperCaseAttribute]
+        [Required]
         public string Titulo { get; set; }
-    
+        public List<Comentario> Comentarios { get; set; }
     }
 }
