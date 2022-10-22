47f54bc066dca00f6c91b71542cac73375ec581d
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-21 21:04:56 -0300

    Create ApplicationDbContext2.0.cs

---
+++ ApplicationDbContext2.0.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Autor> Autores {get; set;}
+        public DbSet<Libro> Libros {get; set;}
+        public DbSet<Comentario> Comentarios { get; set;}
+    }
+}
