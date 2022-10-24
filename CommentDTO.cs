3f6234585e966d2a3b0ba02fbd46cc0bb7296996
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-24 19:23:28 -0300

    Create CommentDTO.cs

---
+++ CommentDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.DTOs
+{
+    public class CommentDTO
+    {
+         public int Id { get; set; }
+        public string Content { get; set; }
+    }
+}
