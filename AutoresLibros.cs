8ea4a443e7f96fd4e55f66ff1e72356a037361f6
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-25 20:59:33 -0300

    Create AutoresLibros.cs
    many to many relation entity

---
+++ AutoresLibros.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.Entidades
+{
+    public class AutoresLibros
+    {
+        public int LibroId { get; set; }
+        public int AutorId { get; set; }
+        public string Orden { get; set; }
+        public Libro Libro { get; set; }
+        public Autor Autor { get; set; }
+    }
+}
