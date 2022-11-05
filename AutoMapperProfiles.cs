9a8d7f5d3439256809934fc79c27ccbf17407779
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-04 22:13:29 -0300

    Update AutoMapperProfiles.cs

--- AutoMapperProfiles.cs
+++ AutoMapperProfiles.cs
@@ -14,10 +14,28 @@ namespace WebAPIAutores.Utilidades
         {
             CreateMap<AutorCreationDTO, Autor>();
             CreateMap<Autor, AutorDTO>();
-            CreateMap<LibroCreationDTO, Libro>();
+            CreateMap<LibroCreationDTO, Libro>()
+                .ForMember(libro => libro.AutoresLibros, options => options.MapFrom(MapAuthorBooks));
             CreateMap<Libro, LibroDTO>();
             CreateMap<CommentCreationDTO, Comentario>();
             CreateMap<Comentario, CommentDTO>();
+        }
+
+        private List<AutoresLibros> MapAuthorBooks(LibroCreationDTO libroCreationDTO, Libro libro)
+        {
+            var result = new List<AutoresLibros>();
+
+            if(libroCreationDTO.AutoresIds == null)
+            {
+                return result;
+            }
+
+            foreach(var autorId in libroCreationDTO.AutoresIds)
+            {
+                result.Add(new AutoresLibros() {AutorId = autorId});
+            }
+
+            return result;
         }
     }
 }
