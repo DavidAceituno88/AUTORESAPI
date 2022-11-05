0b6a98d6f08bac236407a26194ca8f386080493b
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-05 20:44:06 -0300

    Update AutoresLibros.cs

--- AutoresLibros.cs
+++ AutoresLibros.cs
@@ -9,7 +9,7 @@ namespace WebAPIAutores.Entidades
     {
         public int LibroId { get; set; }
         public int AutorId { get; set; }
-        public string Orden { get; set; }
+        public int Orden { get; set; }
         public Libro Libro { get; set; }
         public Autor Autor { get; set; }
     }
