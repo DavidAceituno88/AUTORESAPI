89eb8ac2a4e0e019561417b34a55c5463cc2e978
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-08 20:04:14 -0400

    Create AutorDTO.cs

---
+++ AutorDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.DTOs
+{
+    public class AutorDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
