f41219841001b9a1c39c56e2cfcdad68653b74e7
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-11-08 20:03:37 -0400

    Create AutorDTOWithLibros.cs

---
+++ AutorDTOWithLibros.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIAutores.DTOs
+{
+    public class AutorDTOWithLibros : AutorDTO
+    {       
+        public List<LibroDTO> Libros { get; set; }
+    }
+}
